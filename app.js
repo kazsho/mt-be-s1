34cfb83c32b732d5b3343db2ab9d1d926fa78da8
malak <malookaabdalla@gmail.com>
2024-03-18 12:06:53 +0000

    add routes

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+speechToText = require("./models/SpeechToText");
+
+const logRoutes = require("./middleware/logger");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(logRoutes);
+app.use(bodyParser.raw({ type: "audio/*", limit: "10mb" }));
+
+app.get("/", (req, res) => {
+  res.json({
+    name: "Language app",
+    description: "An app that utilizes AI to help teach languages",
+  });
+});
+
+app.post("/receive", async (req, res) => {
+  try {
+    const audioData = req.body.audio;
+    const transcription = await speechToText(audioData);
+    console.log("Transcription:", transcription);
+    res.send("Audio received and transcribed successfully");
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Error processing audio");
+  }
+});
+
+app.get("/send", (req, res) => {
+  const audioData = generateAudioData();
+  res.set("Content-Type", "audio/mp3");
+  res.send(audioData);
+});
+
+module.exports = app;
---
+++ models/SpeechToText.js
@@ -0,0 +1,38 @@
+const speech = require("@google-cloud/speech");
+
+const client = new speech.SpeechClient({
+  keyFilename: "./key.json",
+});
+
+async function speechToText(audioData) {
+  try {
+    const audioBytes = Buffer.from(audioData, "base64");
+
+    const audio = {
+      content: audioBytes,
+    };
+    const config = {
+      encoding: "FLAC",
+      sampleRateHertz: 48000,
+      languageCode: "gu-IN",
+      audioChannelCount: 1,
+      enableSeparateRecognitionPerChannel: false,
+    };
+    const request = {
+      audio: audio,
+      config: config,
+    };
+
+    const [response] = await client.recognize(request);
+    const transcription = response.results
+      .map((result) => result.alternatives[0].transcript)
+      .join("\n");
+
+    return transcription;
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw error;
+  }
+}
+
+module.exports = speechToText;
